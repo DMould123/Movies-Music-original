2e2e8e376dc6cb28396e9ecbd4333342c3fbde63
DMould123 <95115610+DMould123@users.noreply.github.com>
2023-09-11 16:56:50 +0200

    Commit of first React Movies & Music to upload back to my Portfolio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/NavBar'
+
+export default function App(e) {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  )
+}
